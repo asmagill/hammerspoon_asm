672c5607c65599cc3a06eb9ace909dd5a5d6a775
asmagill <asmagill@users.noreply.github.com>
2015-08-09 04:59:33 -0500

    wip on reusable and extendable NSObject->lua and lua->NSObject conversion tools

--- wip/extras/internal.m
+++ wip/extras/internal.m
@@ -2,25 +2,123 @@
 #import <LuaSkin/LuaSkin.h>
 #import "../hammerspoon.h"
 
-// forward declare so we can use this earlier than we define it:
-static id lua_to_NSObject(lua_State* L, int idx) ;
+#import "objectconversion.m"
 
 /// hs._asm.extras.NSLog(luavalue)
 /// Function
 /// Send a representation of the lua value passed in to the Console application via NSLog.
 static int extras_nslog(lua_State* L) {
-    id val = lua_to_NSObject(L, 1);
+    id val = lua_toNSObject(L, 1);
     NSLog(@"%@", val);
     return 0;
 }
 
-// static int CheckForUpdate(lua_State* L) {
-//     check for [SUUpdater checkForUpdate]
+
+static int extras_defaults(lua_State* L) {
+    NSDictionary *defaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName: [[NSBundle mainBundle] bundleIdentifier]] ;
+    NSObject_tolua(L, defaults) ;
+    return 1;
+}
+
+// A crash waiting to happen, but proof of concept that a true "bridge" might one day be
+// possible... with a crap load of work...  Also check out NSInvocation.  Will need a way to store
+// results and return userdata for most things...
+//
+// static int extras_bridge(lua_State* L) {
+//     NSString *className    = [NSString stringWithUTF8String:luaL_checkstring(L, 1)] ;
+//     NSString *selectorName = [NSString stringWithUTF8String:luaL_checkstring(L, 2)] ;
+//
+//     if (NSClassFromString(className)) {
+//         if ([NSClassFromString(className) respondsToSelector:NSSelectorFromString(selectorName)]) {
+//             NSObject_tolua(L, [NSClassFromString(className) performSelector:NSSelectorFromString(selectorName)]) ;
+//         } else {
+//             printToConsole(L, (char *)[[NSString stringWithFormat:@"Class %@ does not respond to selector %@", className, selectorName] UTF8String]) ;
+//             lua_pushnil(L) ;
+//         }
+//     } else {
+//         printToConsole(L, (char *)[[NSString stringWithFormat:@"Class %@ is not loaded or doesn't exist", className] UTF8String]) ;
+//         lua_pushnil(L) ;
+//     }
+//
+//     return 1 ;
+// }
+
+// /// hs.automaticallyChecksForUpdates([setting]) -> bool
+// /// Function
+// /// Gets and optionally sets the Hammerspoon option to automatically check for updates.
+// ///
+// /// Parameters:
+// ///  * setting - an optional boolean variable indicating if Hammerspoon should (true) or should not (false) check for updates.
+// ///
+// /// Returns:
+// ///  * The current (or newly set) value indicating whether or not automatic update checks should occur for Hammerspoon.
+// ///
+// /// Notes:
+// ///  * If you are running a non-release or locally compiled version of Hammerspoon then the results of this function are unspecified.
+// static int automaticallyChecksForUpdates(lua_State *L) {
+//     if (NSClassFromString(@"SUUpdater")) {
+//         NSString *frameworkPath = [[[NSBundle mainBundle] privateFrameworksPath] stringByAppendingPathComponent:@"Sparkle.framework"];
+//         if ([[NSBundle bundleWithPath:frameworkPath] load]) {
+//             id sharedUpdater = [NSClassFromString(@"SUUpdater")  performSelector:@selector(sharedUpdater)] ;
+//             if (lua_isboolean(L, 1)) {
+//
+//             // This convoluted #$@#% is required (a) because we want to weakly link to the SparkleFramework for dev builds, and
+//             // (b) because performSelector: withObject: only works when withObject: takes an argument of type id or nil
+//
+//             // the following is equivalent to: [sharedUpdater setAutomaticallyChecksForUpdates:lua_toboolean(L, 1)] ;
+//
+//                 BOOL myBoolValue = lua_toboolean(L, 1) ;
+//                 NSMethodSignature * mySignature = [NSClassFromString(@"SUUpdater") instanceMethodSignatureForSelector:@selector(setAutomaticallyChecksForUpdates:)];
+//                 NSInvocation * myInvocation = [NSInvocation invocationWithMethodSignature:mySignature];
+//                 [myInvocation setTarget:sharedUpdater];
+//             // even though signature specifies this, we need to specify it in the invocation, since the signature is re-usable
+//             // for any method which accepts the same signature list for the target.
+//                 [myInvocation setSelector:@selector(setAutomaticallyChecksForUpdates:)];
+//                 [myInvocation setArgument:&myBoolValue atIndex:2];
+//                 [myInvocation invoke];
+//
+//             // whew!
+//
+//             }
+//             lua_pushboolean(L, (BOOL)[sharedUpdater performSelector:@selector(automaticallyChecksForUpdates)]) ;
+//         } else {
+//             printToConsole(L, "-- Sparkle Update framework not available for the running instance of Hammerspoon.") ;
+//             lua_pushboolean(L, NO) ;
+//         }
+//     } else {
+//         printToConsole(L, "-- Sparkle Update framework not available for the running instance of Hammerspoon.") ;
+//         lua_pushboolean(L, NO) ;
+//     }
+//     return 1 ;
 // }
 //
-// likewise for doesCheckForUpdate
+// /// hs.checkForUpdates() -> none
+// /// Function
+// /// Check for an update now, and if one is available, prompt the user to continue the update process.
+// ///
+// /// Parameters:
+// ///  * None
+// ///
+// /// Returns:
+// ///  * None
+// ///
+// /// Notes:
+// ///  * If you are running a non-release or locally compiled version of Hammerspoon then the results of this function are unspecified.
+// static int checkForUpdates(lua_State *L) {
+//     if (NSClassFromString(@"SUUpdater")) {
+//         NSString *frameworkPath = [[[NSBundle mainBundle] privateFrameworksPath] stringByAppendingPathComponent:@"Sparkle.framework"];
+//         if ([[NSBundle bundleWithPath:frameworkPath] load]) {
+//             id sharedUpdater = [NSClassFromString(@"SUUpdater")  performSelector:@selector(sharedUpdater)] ;
 //
-// test under production build and mine.
+//             [sharedUpdater performSelector:@selector(checkForUpdates:) withObject:nil] ;
+//         } else {
+//             printToConsole(L, "-- Sparkle Update framework not available for the running instance of Hammerspoon.") ;
+//         }
+//     } else {
+//         printToConsole(L, "-- Sparkle Update framework not available for the running instance of Hammerspoon.") ;
+//     }
+//     return 0 ;
+// }
 
 @interface MJConsoleWindowController : NSWindowController
 
@@ -52,7 +150,7 @@ static int console_behavior(lua_State* L) {
 /// Returns the userdata object as a binary string. Usually userdata is pretty boring -- containing c pointers, etc.  However, for some of the more complex userdata blobs for callbacks and such this can be useful with hs._asm.extras.hexdump for debugging to see what parts of the structure are actually getting set, etc.
 static int ud_tostring (lua_State *L) {
     void *data = lua_touserdata(L,1);
-    int sz;
+    size_t sz;
     if (data == NULL) {
         lua_pushnil(L);
         lua_pushstring(L,"not a userdata type");
@@ -64,127 +162,7 @@ static int ud_tostring (lua_State *L) {
     }
 }
 
-// The following two functions will go away someday (soon I hope) and be found in the core
-// app of hammerspoon because they are just so darned useful in so many contexts... but they
-// have serious limitations as well, and I need to work to clear those... it's an absolute
-// requirement for this module, and the way this module is being used *shouldn't* trip the
-// issues unless someone absolutely tries to screw them up... and all it does is
-// crash Hammerspoon when it happens, so...
-
-static id lua_to_NSObject(lua_State* L, int idx) {
-    idx = lua_absindex(L,idx);
-    switch (lua_type(L, idx)) {
-        case LUA_TNUMBER: return @(lua_tonumber(L, idx));
-        case LUA_TSTRING: return [NSString stringWithUTF8String: lua_tostring(L, idx)];
-        case LUA_TNIL: return [NSNull null];
-        case LUA_TBOOLEAN: return lua_toboolean(L, idx) ? (id)kCFBooleanTrue : (id)kCFBooleanFalse;
-        case LUA_TTABLE: {
-            NSMutableDictionary* numerics    = [NSMutableDictionary dictionary];
-            NSMutableDictionary* nonNumerics = [NSMutableDictionary dictionary];
-            NSMutableIndexSet*   numericKeys = [NSMutableIndexSet indexSet];
-            NSMutableArray*      numberArray = [NSMutableArray array];
-            lua_pushnil(L);
-            while (lua_next(L, idx) != 0) {
-                id key = lua_to_NSObject(L, -2);
-                id val = lua_to_NSObject(L, lua_gettop(L));
-                if ([key isKindOfClass: [NSNumber class]]) {
-                    [numericKeys addIndex:[key intValue]];
-                    [numerics setValue:val forKey:key];
-                } else {
-                    [nonNumerics setValue:val forKey:key];
-                }
-                lua_pop(L, 1);
-            }
-            if (numerics.count > 0) {
-                for (unsigned long i = 1; i <= [numericKeys lastIndex]; i++) {
-                    [numberArray addObject:(
-                        [numerics objectForKey:[NSNumber numberWithInteger:i]] ?
-                            [numerics objectForKey:[NSNumber numberWithInteger:i]] : [NSNull null]
-                    )];
-                }
-                if (nonNumerics.count == 0)
-                    return [numberArray copy];
-            } else {
-                return [nonNumerics copy];
-            }
-            NSMutableDictionary* unionBlob = [NSMutableDictionary dictionary];
-            [unionBlob setValue:[NSArray arrayWithObjects:numberArray, nonNumerics, nil] forKey:@"MJ_LUA_TABLE"];
-            return [unionBlob copy];
-        }
-        default: { lua_pushliteral(L, "non-serializable object"); lua_error(L); }
-    }
-    return nil;
-}
-
-// static void NSObject_to_lua(lua_State* L, id obj) {
-//     if (obj == nil || [obj isEqual: [NSNull null]]) { lua_pushnil(L); }
-//     else if ([obj isKindOfClass: [NSDictionary class]]) {
-//         BOOL handled = NO;
-//         if ([obj count] == 1) {
-//             if ([obj objectForKey:@"MJ_LUA_NIL"]) {
-//                 lua_pushnil(L);
-//                 handled = YES;
-//             } else
-//             if ([obj objectForKey:@"MJ_LUA_TABLE"]) {
-//                 NSArray* parts = [obj objectForKey:@"MJ_LUA_TABLE"] ;
-//                 NSArray* numerics = [parts objectAtIndex:0] ;
-//                 NSDictionary* nonNumerics = [parts objectAtIndex:1] ;
-//                 lua_newtable(L);
-//                 int i = 0;
-//                 for (id item in numerics) {
-//                     NSObject_to_lua(L, item);
-//                     lua_rawseti(L, -2, ++i);
-//                 }
-//                 NSArray *keys = [nonNumerics allKeys];
-//                 NSArray *values = [nonNumerics allValues];
-//                 for (unsigned long i = 0; i < keys.count; i++) {
-//                     NSObject_to_lua(L, [keys objectAtIndex:i]);
-//                     NSObject_to_lua(L, [values objectAtIndex:i]);
-//                     lua_settable(L, -3);
-//                 }
-//                 handled = YES;
-//             }
-//         }
-//         if (!handled) {
-//             NSArray *keys = [obj allKeys];
-//             NSArray *values = [obj allValues];
-//             lua_newtable(L);
-//             for (unsigned long i = 0; i < keys.count; i++) {
-//                 NSObject_to_lua(L, [keys objectAtIndex:i]);
-//                 NSObject_to_lua(L, [values objectAtIndex:i]);
-//                 lua_settable(L, -3);
-//             }
-//         }
-//     } else if ([obj isKindOfClass: [NSNumber class]]) {
-//         NSNumber* number = obj;
-//         if (number == (id)kCFBooleanTrue)
-//             lua_pushboolean(L, YES);
-//         else if (number == (id)kCFBooleanFalse)
-//             lua_pushboolean(L, NO);
-//         else
-//             lua_pushnumber(L, [number doubleValue]);
-//     } else if ([obj isKindOfClass: [NSString class]]) {
-//         NSString* string = obj;
-//         lua_pushstring(L, [string UTF8String]);
-//     } else if ([obj isKindOfClass: [NSArray class]]) {
-//         int i = 0;
-//         NSArray* list = obj;
-//         lua_newtable(L);
-//         for (id item in list) {
-//             NSObject_to_lua(L, item);
-//             lua_rawseti(L, -2, ++i);
-//         }
-//     } else if ([obj isKindOfClass: [NSDate class]]) {
-//         lua_pushnumber(L, [(NSDate *) obj timeIntervalSince1970]);
-//     } else if ([obj isKindOfClass: [NSData class]]) {
-//         lua_pushlstring(L, [obj bytes], [obj length]) ;
-//     } else {
-//         lua_pushstring(L, [[NSString stringWithFormat:@"<Object> : %@", obj] UTF8String]) ;
-//     }
-// }
-
 #define get_app(L, idx) *((AXUIElementRef*)luaL_checkudata(L, idx, "hs.application"))
-
 
 // Internal helper function for getMenuArray
 static void _buildMenuArray(lua_State* L, AXUIElementRef app, AXUIElementRef menuItem) {
@@ -262,8 +240,11 @@ static int getMenuArray(lua_State *L) {
 static const luaL_Reg extrasLib[] = {
     {"consoleBehavior",     console_behavior },
     {"NSLog",               extras_nslog },
+    {"defaults",            extras_defaults },
     {"userDataToString",    ud_tostring},
     {"getMenuArray",        getMenuArray},
+//     {"automaticallyChecksForUpdates",     automaticallyChecksForUpdates},
+//     {"checkForUpdates",  checkForUpdates},
     {NULL,                  NULL}
 };
 
