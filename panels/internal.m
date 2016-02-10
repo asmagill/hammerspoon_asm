dfed0d9c754060eba0244da8bb03a81970d08fcb
asmagill <asmagill@users.noreply.github.com>
2016-02-09 20:36:49 -0600

    fontPanel is annoying when not tied to a nstextview

--- panels/internal.m
+++ panels/internal.m
@@ -22,7 +22,7 @@ static int refTable = LUA_NOREF ;
         _callbackRef = LUA_NOREF ;
         NSColorPanel *cp = [NSColorPanel sharedColorPanel];
         [cp setTarget:self];
-        [cp setAction:@selector(colorUpdate:)];
+        [cp setAction:@selector(colorCallback:)];
 
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(colorClose:)
@@ -51,7 +51,7 @@ static int refTable = LUA_NOREF ;
     }
 }
 
-- (void)colorUpdate:(NSColorPanel*)colorPanel {
+- (void)colorCallback:(NSColorPanel*)colorPanel {
     if (_callbackRef != LUA_NOREF) {
         dispatch_async(dispatch_get_main_queue(), ^{
             LuaSkin   *skin = [LuaSkin shared] ;
@@ -70,25 +70,106 @@ static int refTable = LUA_NOREF ;
 }
 @end
 
+@interface HSFontPanel : NSObject
+@property int callbackRef ;
+@end
+
+@implementation HSFontPanel
+- (instancetype)init {
+    self = [super init] ;
+    if (self) {
+        _callbackRef = LUA_NOREF ;
+        NSFontPanel *fp = [NSFontPanel sharedFontPanel];
+        NSFontManager *fm = [NSFontManager sharedFontManager];
+        [fm setTarget:self];
+        [fm setSelectedFont:[NSFont systemFontOfSize: 27] isMultiple:NO] ;
+        [fm setSelectedAttributes:@{} isMultiple:NO] ;
+
+//         [fp setAction:@selector(fontCallback:)];
+
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(fontClose:)
+                                                     name:NSWindowWillCloseNotification
+                                                   object:fp] ;
+    }
+    return self ;
+}
+
+- (void)fontClose:(__unused NSNotification*)note {
+    if (_callbackRef != LUA_NOREF) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            LuaSkin   *skin = [LuaSkin shared] ;
+            lua_State *L    = [skin L] ;
+            [skin pushLuaRef:refTable ref:_callbackRef] ;
+            [skin pushNSObject:[[NSFontManager sharedFontManager] selectedFont]] ;
+            lua_pushboolean(L, YES) ;
+            if (![skin protectedCallAndTraceback:2 nresults:0]) {
+                [skin logError:[NSString stringWithFormat:@"%s: font callback error, %s",
+                                                          USERDATA_TAG,
+                                                          lua_tostring(L, -1)]] ;
+                lua_pop(L, 1) ;
+            }
+        }) ;
+    }
+}
+
+- (void)changeFont:(id)obj {
+    if (_callbackRef != LUA_NOREF) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            LuaSkin   *skin = [LuaSkin shared] ;
+            lua_State *L    = [skin L] ;
+            [skin pushLuaRef:refTable ref:_callbackRef] ;
+            [skin pushNSObject:[obj selectedFont]] ;
+            lua_pushboolean(L, NO) ;
+            if (![skin protectedCallAndTraceback:2 nresults:0]) {
+                [skin logError:[NSString stringWithFormat:@"%s: font callback error, %s",
+                                                          USERDATA_TAG,
+                                                          lua_tostring(L, -1)]] ;
+                lua_pop(L, 1) ;
+            }
+        }) ;
+    }
+}
+
+- (void)changeAttributes:(id)obj {
+    if (_callbackRef != LUA_NOREF) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            LuaSkin   *skin = [LuaSkin shared] ;
+            lua_State *L    = [skin L] ;
+            [skin pushLuaRef:refTable ref:_callbackRef] ;
+            [skin pushNSObject:[obj convertAttributes:@{}]] ;
+            lua_pushboolean(L, NO) ;
+            if (![skin protectedCallAndTraceback:2 nresults:0]) {
+                [skin logError:[NSString stringWithFormat:@"%s: font callback error, %s",
+                                                          USERDATA_TAG,
+                                                          lua_tostring(L, -1)]] ;
+                lua_pop(L, 1) ;
+            }
+        }) ;
+    }
+}
+
+@end
+
 #pragma mark - Color Panel Functions
 
-static HSColorPanel *panelReceiverObject ;
+static HSColorPanel *cpReceiverObject ;
 
 static int colorPanelCallback(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TFUNCTION | LS_TNIL | LS_TOPTIONAL, LS_TBREAK] ;
 
-    if (panelReceiverObject.callbackRef != LUA_NOREF) {
-        [skin pushLuaRef:refTable ref:panelReceiverObject.callbackRef] ;
+    if (cpReceiverObject.callbackRef != LUA_NOREF) {
+        [skin pushLuaRef:refTable ref:cpReceiverObject.callbackRef] ;
     } else {
         lua_pushnil(L) ;
     }
     if (lua_gettop(L) == 2) { // we just added to it...
         // in either case, we need to remove an existing callback, so...
-        panelReceiverObject.callbackRef = [skin luaUnref:refTable ref:panelReceiverObject.callbackRef] ;
+        cpReceiverObject.callbackRef = [skin luaUnref:refTable ref:cpReceiverObject.callbackRef] ;
         if (lua_type(L, 1) == LUA_TFUNCTION) {
             lua_pushvalue(L, 1) ;
-            panelReceiverObject.callbackRef = [skin luaRef:refTable] ;
+            cpReceiverObject.callbackRef = [skin luaRef:refTable] ;
         }
     }
     // return the *last* fn (or nil) so you can save it and re-attach it if something needs to
@@ -196,19 +277,70 @@ static int colorPanelHide(__unused lua_State *L) {
     return 0 ;
 }
 
-static int releaseColorPanelReceiver(__unused lua_State *L) {
-    NSColorPanel *cp = [NSColorPanel sharedColorPanel];
-    [[NSNotificationCenter defaultCenter] removeObserver:panelReceiverObject
-                                                    name:NSWindowWillCloseNotification
-                                                  object:cp] ;
-    [cp setTarget:nil];
-    [cp setAction:nil];
-    [cp close];
-    panelReceiverObject = nil ;
+#pragma mark - Font Panel Functions
+
+static HSFontPanel *fpReceiverObject ;
+
+static int fontPanelCallback(lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    [skin checkArgs:LS_TFUNCTION | LS_TNIL | LS_TOPTIONAL, LS_TBREAK] ;
+
+    if (fpReceiverObject.callbackRef != LUA_NOREF) {
+        [skin pushLuaRef:refTable ref:fpReceiverObject.callbackRef] ;
+    } else {
+        lua_pushnil(L) ;
+    }
+    if (lua_gettop(L) == 2) { // we just added to it...
+        // in either case, we need to remove an existing callback, so...
+        fpReceiverObject.callbackRef = [skin luaUnref:refTable ref:fpReceiverObject.callbackRef] ;
+        if (lua_type(L, 1) == LUA_TFUNCTION) {
+            lua_pushvalue(L, 1) ;
+            fpReceiverObject.callbackRef = [skin luaRef:refTable] ;
+        }
+    }
+    // return the *last* fn (or nil) so you can save it and re-attach it if something needs to
+    // temporarily take the callbacks
+    return 1 ;
+}
+
+static int fontPanelShow(__unused lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    [skin checkArgs:LS_TBREAK] ;
+    [NSApp orderFrontFontPanel:nil] ;
+    return 0 ;
+}
+
+static int fontPanelHide(__unused lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    [skin checkArgs:LS_TBREAK] ;
+    [[NSFontPanel sharedFontPanel] close] ;
     return 0 ;
 }
 
 #pragma mark - Hammerspoon/Lua Infrastructure
+
+static int releaseReceivers(__unused lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    NSColorPanel *cp = [NSColorPanel sharedColorPanel];
+    [[NSNotificationCenter defaultCenter] removeObserver:cpReceiverObject
+                                                    name:NSWindowWillCloseNotification
+                                                  object:cp] ;
+    [cp setTarget:nil];
+    [cp setAction:nil];
+    if (cpReceiverObject.callbackRef != LUA_NOREF) [skin luaUnref:refTable ref:cpReceiverObject.callbackRef] ;
+    [cp close];
+    cpReceiverObject = nil ;
+
+    NSFontPanel *fp = [NSFontPanel sharedFontPanel];
+    NSFontManager *fm = [NSFontManager sharedFontManager];
+    [[NSNotificationCenter defaultCenter] removeObserver:fpReceiverObject
+                                                    name:NSWindowWillCloseNotification
+                                                  object:fp] ;
+    if (fpReceiverObject.callbackRef != LUA_NOREF) [skin luaUnref:refTable ref:fpReceiverObject.callbackRef] ;
+    [fm setTarget:nil] ;
+    fpReceiverObject = nil ;
+    return 0 ;
+}
 
 static luaL_Reg moduleLib[] = {
     {NULL,    NULL}
@@ -226,8 +358,16 @@ static luaL_Reg colorPanelLib[] = {
     {NULL,         NULL}
 };
 
+static luaL_Reg fontPanelLib[] = {
+    {"show",     fontPanelShow},
+    {"hide",     fontPanelHide},
+    {"callback", fontPanelCallback},
+
+    {NULL,   NULL}
+};
+
 static luaL_Reg module_metaLib[] = {
-    {"__gc", releaseColorPanelReceiver},
+    {"__gc", releaseReceivers},
     {NULL,   NULL}
 };
 
@@ -235,9 +375,11 @@ int luaopen_hs__asm_panels_internal(lua_State* __unused L) {
     LuaSkin *skin = [LuaSkin shared];
     refTable = [skin registerLibrary:moduleLib metaFunctions:module_metaLib] ;
 
-    [NSColorPanel setPickerMask:NSColorPanelAllModesMask] ;
-    panelReceiverObject = [[HSColorPanel alloc] init] ;
     luaL_newlib(L, colorPanelLib) ; lua_setfield(L, -2, "color") ;
+    [NSColorPanel setPickerMask:NSColorPanelAllModesMask] ;
+    cpReceiverObject = [[HSColorPanel alloc] init] ;
+    fpReceiverObject = [[HSFontPanel alloc] init] ;
+    luaL_newlib(L, fontPanelLib) ;  lua_setfield(L, -2, "font") ;
 
     return 1;
 }
