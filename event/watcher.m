762d4e33f670a1077cf77267337a623a12298030
asmagill <asmagill@users.noreply.github.com>
2017-03-26 03:25:31 -0500

    inching along

--- event/watcher.m
+++ event/watcher.m
@@ -7,7 +7,7 @@ static const char * const USERDATA_TAG = "hs.events.watcher" ;
 static int refTable = LUA_NOREF;
 static HSEventThreadManager *threadManager ;
 
-// #define get_objectFromUserdata(objType, L, idx, tag) (objType*)*((void**)luaL_checkudata(L, idx, tag))
+#define get_objectFromUserdata(objType, L, idx, tag) (objType*)*((void**)luaL_checkudata(L, idx, tag))
 // #define get_structFromUserdata(objType, L, idx, tag) ((objType *)luaL_checkudata(L, idx, tag))
 // #define get_cfobjectFromUserdata(objType, L, idx, tag) *((objType *)luaL_checkudata(L, idx, tag))
 
@@ -60,6 +60,11 @@ static NSString *CGErrorToString(CGError error) {
 @property NSThread            *watcherThread ;
 @property NSRunLoop           *threadRunLoop ;
 @property NSTimer             *keepAliveTimer ;
+@end
+
+@interface HSEventWatcherObject : NSObject
+@property int  fnRef ;
+@property BOOL passive ;
 @end
 
 @implementation HSEventThreadManager
@@ -126,12 +131,44 @@ static NSString *CGErrorToString(CGError error) {
 
 @end
 
+@implementation HSEventWatcherObject
+
+- (instancetype)init {
+    self = [super init] ;
+    if (self) {
+        _fnRef   = LUA_NOREF ;
+        _passive = YES ;
+    }
+    return self ;
+}
+
+- (instancetype)initWithFunction:(int)fnRef passive:(BOOL)passive {
+    self = [self init] ;
+    if (self) {
+        _fnRef   = fnRef ;
+        _passive = passive ;
+    }
+    return self ;
+}
+
+@end
+
 #pragma mark - Module Functions
 
-// static int watcher_new(lua_State *L) {
-//     LuaSkin *skin = [LuaSkin shared] ;
-//     [skin checkArgs:LS_TTABLE, LS_TFUNCTION, LS_TBOOLEAN | LS_TOPTIONAL, LS_TBREAK] ;
-// }
+static int watcher_new(lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    [skin checkArgs:LS_TFUNCTION | LS_TNIL, LS_TBOOLEAN | LS_TOPTIONAL, LS_TBREAK] ;
+
+    BOOL passive = (lua_gettop(L) == 2) ? (BOOL)lua_toboolean(L, 2) : YES ;
+    int fnRef = LUA_NOREF ;
+    if (lua_type(L, 1) == LUA_TFUNCTION) {
+        lua_pushvalue(L, 1) ;
+        fnRef = [skin luaRef:refTable] ;
+    }
+
+    [skin pushNSObject:[[HSEventWatcherObject alloc] initWithFunction:fnRef passive:passive]] ;
+    return 1 ;
+}
 
 static int watcher_watchers(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
@@ -216,59 +253,64 @@ static int watcher_watchers(lua_State *L) {
 // These must not throw a lua error to ensure LuaSkin can safely be used from Objective-C
 // delegates and blocks.
 
-// static int push<moduleType>(lua_State *L, id obj) {
-//     <moduleType> *value = obj;
-//     void** valuePtr = lua_newuserdata(L, sizeof(<moduleType> *));
-//     *valuePtr = (__bridge_retained void *)value;
-//     luaL_getmetatable(L, USERDATA_TAG);
-//     lua_setmetatable(L, -2);
-//     return 1;
-// }
-//
-// id to<moduleType>FromLua(lua_State *L, int idx) {
-//     LuaSkin *skin = [LuaSkin shared] ;
-//     <moduleType> *value ;
-//     if (luaL_testudata(L, idx, USERDATA_TAG)) {
-//         value = get_objectFromUserdata(__bridge <moduleType>, L, idx, USERDATA_TAG) ;
-//     } else {
-//         [skin logError:[NSString stringWithFormat:@"expected %s object, found %s", USERDATA_TAG,
-//                                                    lua_typename(L, lua_type(L, idx))]] ;
-//     }
-//     return value ;
-// }
+static int pushHSEventWatcherObject(lua_State *L, id obj) {
+    HSEventWatcherObject *value = obj;
+    void** valuePtr = lua_newuserdata(L, sizeof(HSEventWatcherObject *));
+    *valuePtr = (__bridge_retained void *)value;
+    luaL_getmetatable(L, USERDATA_TAG);
+    lua_setmetatable(L, -2);
+    return 1;
+}
+
+id toHSEventWatcherObjectFromLua(lua_State *L, int idx) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    HSEventWatcherObject *value ;
+    if (luaL_testudata(L, idx, USERDATA_TAG)) {
+        value = get_objectFromUserdata(__bridge HSEventWatcherObject, L, idx, USERDATA_TAG) ;
+    } else {
+        [skin logError:[NSString stringWithFormat:@"expected %s object, found %s", USERDATA_TAG,
+                                                   lua_typename(L, lua_type(L, idx))]] ;
+    }
+    return value ;
+}
 
 #pragma mark - Hammerspoon/Lua Infrastructure
 
-// static int userdata_tostring(lua_State* L) {
-//     LuaSkin *skin = [LuaSkin shared] ;
-//     <moduleType> *obj = [skin luaObjectAtIndex:1 toClass:"<moduleType>"] ;
-//     NSString *title = ... ;
-//     [skin pushNSObject:[NSString stringWithFormat:@"%s: %@ (%p)", USERDATA_TAG, title, lua_topointer(L, 1)]] ;
-//     return 1 ;
-// }
+static int userdata_tostring(lua_State* L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    HSEventWatcherObject *obj = [skin luaObjectAtIndex:1 toClass:"HSEventWatcherObject"] ;
+    NSString *title = obj.passive ? @"passive" : @"active" ;
+    [skin pushNSObject:[NSString stringWithFormat:@"%s: %@ (%p)", USERDATA_TAG, title, lua_topointer(L, 1)]] ;
+    return 1 ;
+}
 
-// static int userdata_eq(lua_State* L) {
-// // can't get here if at least one of us isn't a userdata type, and we only care if both types are ours,
-// // so use luaL_testudata before the macro causes a lua error
-//     if (luaL_testudata(L, 1, USERDATA_TAG) && luaL_testudata(L, 2, USERDATA_TAG)) {
-//         LuaSkin *skin = [LuaSkin shared] ;
-//         <moduleType> *obj1 = [skin luaObjectAtIndex:1 toClass:"<moduleType>"] ;
-//         <moduleType> *obj2 = [skin luaObjectAtIndex:2 toClass:"<moduleType>"] ;
-//         lua_pushboolean(L, [obj1 isEqualTo:obj2]) ;
-//     } else {
-//         lua_pushboolean(L, NO) ;
-//     }
-//     return 1 ;
-// }
+static int userdata_eq(lua_State* L) {
+// can't get here if at least one of us isn't a userdata type, and we only care if both types are ours,
+// so use luaL_testudata before the macro causes a lua error
+    if (luaL_testudata(L, 1, USERDATA_TAG) && luaL_testudata(L, 2, USERDATA_TAG)) {
+        LuaSkin *skin = [LuaSkin shared] ;
+        HSEventWatcherObject *obj1 = [skin luaObjectAtIndex:1 toClass:"HSEventWatcherObject"] ;
+        HSEventWatcherObject *obj2 = [skin luaObjectAtIndex:2 toClass:"HSEventWatcherObject"] ;
+        lua_pushboolean(L, [obj1 isEqualTo:obj2]) ;
+    } else {
+        lua_pushboolean(L, NO) ;
+    }
+    return 1 ;
+}
 
-// static int userdata_gc(lua_State* L) {
-//     <moduleType> *obj = get_objectFromUserdata(__bridge_transfer <moduleType>, L, 1, USERDATA_TAG) ;
-//     if (obj) obj = nil ;
-//     // Remove the Metatable so future use of the variable in Lua won't think its valid
-//     lua_pushnil(L) ;
-//     lua_setmetatable(L, 1) ;
-//     return 0 ;
-// }
+static int userdata_gc(lua_State* L) {
+    HSEventWatcherObject *obj = get_objectFromUserdata(__bridge_transfer HSEventWatcherObject, L, 1, USERDATA_TAG) ;
+    if (obj) {
+        LuaSkin *skin = [LuaSkin shared] ;
+        obj.fnRef = [skin luaUnref:refTable ref:obj.fnRef] ;
+        obj = nil ;
+    }
+
+    // Remove the Metatable so future use of the variable in Lua won't think its valid
+    lua_pushnil(L) ;
+    lua_setmetatable(L, 1) ;
+    return 0 ;
+}
 
 static int meta_gc(lua_State* __unused L) {
     if (threadManager) [threadManager cancelThread:nil];
@@ -278,14 +320,15 @@ static int meta_gc(lua_State* __unused L) {
 
 // Metatable for userdata objects
 static const luaL_Reg userdata_metaLib[] = {
-//     {"__tostring", userdata_tostring},
-//     {"__eq",       userdata_eq},
-//     {"__gc",       userdata_gc},
+    {"__tostring", userdata_tostring},
+    {"__eq",       userdata_eq},
+    {"__gc",       userdata_gc},
     {NULL,         NULL}
 };
 
 // Functions for returned object when module loads
 static luaL_Reg moduleLib[] = {
+    {"new",      watcher_new},
     {"watchers", watcher_watchers},
     {NULL,       NULL}
 };
@@ -296,7 +339,6 @@ static const luaL_Reg module_metaLib[] = {
     {NULL,   NULL}
 };
 
-// NOTE: ** Make sure to change luaopen_..._internal **
 int luaopen_hs_event_watcher(lua_State* __unused L) {
     LuaSkin *skin = [LuaSkin shared] ;
     refTable = [skin registerLibraryWithObject:USERDATA_TAG
@@ -305,12 +347,10 @@ int luaopen_hs_event_watcher(lua_State* __unused L) {
                                objectFunctions:userdata_metaLib];
 
     threadManager = [[HSEventThreadManager alloc] init] ;
-//     [skin registerPushNSHelper:push<moduleType>         forClass:"<moduleType>"];
 
-// // one, but not both, of...
-//     [skin registerLuaObjectHelper:to<moduleType>FromLua forClass:"<moduleType>"
-//                                              withUserdataMapping:USERDATA_TAG];
-//     [skin registerLuaObjectHelper:to<moduleType>FromLua forClass:"<moduleType>"];
+    [skin registerPushNSHelper:pushHSEventWatcherObject         forClass:"HSEventWatcherObject"];
+    [skin registerLuaObjectHelper:toHSEventWatcherObjectFromLua forClass:"HSEventWatcherObject"
+                                             withUserdataMapping:USERDATA_TAG];
 
     return 1;
 }
