0955d2816d47f99c357cd2dedb82caf7d2dde314
asmagill <asmagill@users.noreply.github.com>
2017-11-08 00:36:58 -0600

    slider docs and some bug fixes

--- guitk/element/slider.m
+++ guitk/element/slider.m
@@ -75,6 +75,18 @@ static int refTable = LUA_NOREF;
 
 #pragma mark - Module Functions
 
+/// hs._asm.guitk.element.slider.new([frame]) -> sliderObject
+/// Constructor
+/// Creates a new slider element for `hs._asm.guitk`.
+///
+/// Parameters:
+///  * `frame` - an optional frame table specifying the position and size of the frame for the element.
+///
+/// Returns:
+///  * the sliderObject
+///
+/// Notes:
+///  * In most cases, setting the frame is not necessary and will be overridden when the element is assigned to a manager or to a `hs._asm.guitk` window.
 static int slider_new(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TTABLE | LS_TOPTIONAL, LS_TBREAK] ;
@@ -93,6 +105,18 @@ static int slider_new(lua_State *L) {
 
 #pragma mark - Module Methods
 
+/// hs._asm.guitk.element.slider:tickMarksOnly([state]) -> sliderObject | boolean
+/// Method
+/// Get or set whether the slider limits values to those specified by tick marks or allows selecting a value between tick marks.
+///
+/// Parameters:
+///  * `state` - an optional boolean, default false, specifying whether or not the slider is limited to discrete values indicated by the tick marks (true) or allows values in between as well (false).
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * has no effect if [hs._asm.guitk.element.slider:tickMarks](#tickMarks) is 0
 static int slider_allowsTickMarkValuesOnly(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TBOOLEAN | LS_TOPTIONAL, LS_TBREAK] ;
@@ -107,6 +131,18 @@ static int slider_allowsTickMarkValuesOnly(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:altClickIncrement([value]) -> sliderObject | number
+/// Method
+/// Get or set the amount the slider will move if the user holds down the alt (option) key while clicking on it.
+///
+/// Parameters:
+///  * `value` - an optional number greater than or equal to 0 specifying the amount the slider will move when the user holds down the alt (option) key while clicking on it.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * If this value is 0, holding down the alt (option) key while clicking on the slider has the same effect that not holding down the modifier does: the slider jumps to the position where the click occurs.
 static int slider_altIncrementValue(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TOPTIONAL, LS_TBREAK] ;
@@ -122,6 +158,19 @@ static int slider_altIncrementValue(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:value([value]) -> sliderObject | number
+/// Method
+/// Get or set the current value of the slider, adjusting the knob position if necessary.
+///
+/// Parameters:
+///  * `value` - an optional number specifying the value for the slider.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * If the value is less than [hs._asm.guitk.element.slider:min](#min), then it will be set to the minimum instead.
+///  * If the value is greater than [hs._asm.guitk.element.slider:max](#max), then it will be set to the maximum instead.
 static int slider_currentValue(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TOPTIONAL, LS_TBREAK] ;
@@ -136,6 +185,18 @@ static int slider_currentValue(lua_State *L) {
     return  1 ;
 }
 
+/// hs._asm.guitk.element.slider:max([value]) -> sliderObject | number
+/// Method
+/// Get or set the maximum value the slider can represent.
+///
+/// Parameters:
+///  * `value` - an optional number (default 1.0) specifying the maximum value for the slider.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * If this value is less than [hs._asm.guitk.element.slider:min](#min), the behavior of the slider is undefined.
 static int slider_maxValuee(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TOPTIONAL, LS_TBREAK] ;
@@ -150,6 +211,18 @@ static int slider_maxValuee(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:min([value]) -> sliderObject | number
+/// Method
+/// Get or set the minimum value the slider can represent.
+///
+/// Parameters:
+///  * `value` - an optional number (default 0.0) specifying the minimum value for the slider.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * If this value is greater than [hs._asm.guitk.element.slider:max](#max), the behavior of the slider is undefined.
 static int slider_minValue(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TOPTIONAL, LS_TBREAK] ;
@@ -163,6 +236,20 @@ static int slider_minValue(lua_State *L) {
     }
     return 1 ;
 }
+
+/// hs._asm.guitk.element.slider:tickMarks([marks]) -> sliderObject | integer
+/// Method
+/// Get or set the number of tick marks for the slider.
+///
+/// Parameters:
+///  * `marks` - an optional integer (default 0) specifying the number of tick marks for the slider.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * If the slider is linear, the tick marks will be arranged at equal intervals along the slider. If the slider is circular, a single tick mark will be displayed at the top of the slider for any number passed in that is greater than 0 -- see [hs._asm.guitk.element.slider:type](#type).
+///  * A circular slider with [hs._asm.guitk.element.slider:tickMarksOnly](#tickMarksOnly) set to true will still be limited to the number of discrete intervals specified by the value set by this method, even though the specific tick marks are not visible.
 static int slider_numberOfTickMarks(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TINTEGER | LS_TOPTIONAL, LS_TBREAK] ;
@@ -178,6 +265,18 @@ static int slider_numberOfTickMarks(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:type([type]) -> sliderObject | string
+/// Method
+/// Get or set whether the slider is linear or circular.
+///
+/// Parameters:
+///  * `type` - an optional string, default "linear", specifying whether the slider is circular ("circular") or linear ("linear")
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * The length of a linear slider will expand to fill the dimension appropriate based on the value of [hs._asm.guitk.element.slider:vertical](#vertical); a circular slider will be anchored to the lower right corner of the element's frame.
 static int slider_sliderType(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TSTRING | LS_TOPTIONAL, LS_TBREAK] ;
@@ -209,6 +308,19 @@ static int slider_sliderType(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:tickMarkLocation([location]) -> sliderObject | string
+/// Method
+/// Get or set where tick marks are displayed for the slider.
+///
+/// Parameters:
+///  * `location` - an optional string, default "trailing", specifying whether the tick marks are displayed to the left/below ("trailing") the slider or to the right/above ("leading") the slider.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * This method has no effect on a circular slider -- see [hs._asm.guitk.element.slider:type](#type).
+///  * If [hs._asm.guitk.element.slider:tickMarks](#tickMarks) is 0, this method has no effect.
 static int slider_tickMarkPosition(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TSTRING | LS_TOPTIONAL, LS_TBREAK] ;
@@ -242,15 +354,28 @@ static int slider_tickMarkPosition(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:trackFillColor([color]) -> sliderObject | table
+/// Method
+/// Get or set the color of the slider track in appearances that support it.
+///
+/// Parameters:
+///  * `color` - a color table as defined in `hs.drawing.color`, or explicit nil to reset to the default, specifying the color of the track for the slider.
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * This method is only available in macOS 10.12.1 and newer.
+///  * This method currently appears to have no effect on the visual appearance on the slider; as it was added to the macOS API in 10.12.1, it is suspected that this may be supported in the future and is included here for when that happens.
 static int slider_trackFillColor(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
-    [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TTABLE | LS_TOPTIONAL, LS_TBREAK] ;
+    [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TTABLE | LS_TNIL | LS_TOPTIONAL, LS_TBREAK] ;
     HSASMGUITKElementSlider *slider = [skin toNSObjectAtIndex:1] ;
 
     if (lua_gettop(L) == 1) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunguarded-availability"
-        if ([slider respondsToSelector:@selector(trackFillColor)]) {
+        if ([slider respondsToSelector:NSSelectorFromString(@"trackFillColor")]) {
             [skin pushNSObject:slider.trackFillColor] ;
         } else {
             lua_pushnil(L) ;
@@ -259,8 +384,12 @@ static int slider_trackFillColor(lua_State *L) {
     } else {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunguarded-availability"
-        if ([slider respondsToSelector:@selector(trackFillColor)]) {
-            slider.trackFillColor = [skin luaObjectAtIndex:2 toClass:"NSColor"] ;
+        if ([slider respondsToSelector:NSSelectorFromString(@"trackFillColor")]) {
+            if (lua_type(L, 2) == LUA_TNIL) {
+                slider.trackFillColor = nil ;
+            } else {
+                slider.trackFillColor = [skin luaObjectAtIndex:2 toClass:"NSColor"] ;
+            }
         } else {
             [skin logWarn:[NSString stringWithFormat:@"%s:trackFillColor - requries macOS 10.12.1 or later", USERDATA_TAG]] ;
         }
@@ -270,6 +399,18 @@ static int slider_trackFillColor(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:knobThickness() -> number
+/// Method
+/// Get the thickness of the knob on the slider.
+///
+/// Parameters:
+///  * None
+///
+/// Returns:
+///  * a number specifying the thickness of the slider's knob in pixels.
+///
+/// Notes:
+///  * The thickness is defined to be the extent of the knob along the long dimension of the bar. In a vertical slider, a knob’s thickness is its height; in a horizontal slider, a knob’s thickness is its width.
 static int slider_knobThickness(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TBREAK] ;
@@ -279,6 +420,18 @@ static int slider_knobThickness(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:vertical([state]) -> sliderObject | boolean
+/// Method
+/// Get or set whether a linear slider is vertical or horizontal.
+///
+/// Parameters:
+///  * `state` - an optional boolean, default false, specifying whether or not a linear slider is vertical (true) or horizontal (false).
+///
+/// Returns:
+///  * if a value is provided, returns the sliderObject ; otherwise returns the current value.
+///
+/// Notes:
+///  * This method has no effect on a circular slider -- see [hs._asm.guitk.element.slider:type](#type).
 static int slider_vertical(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TBOOLEAN | LS_TOPTIONAL, LS_TBREAK] ;
@@ -297,6 +450,15 @@ static int slider_vertical(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:tickMarkValue(mark) -> number
+/// Method
+/// Get the value represented by the specified tick mark.
+///
+/// Parameters:
+///  * `mark` - an integer, between 1 and [hs._asm.guitk.element.slider:tickMarks](#tickMarks), specifying the tick mark to get the slider value of.
+///
+/// Returns:
+///  * the number represented by the specified tick mark.
 static int slider_tickMarkValueAtIndex(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TINTEGER, LS_TBREAK] ;
@@ -315,6 +477,19 @@ static int slider_tickMarkValueAtIndex(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:closestTickMarkValue(value) -> number
+/// Method
+/// Get the value of the tick mark closest to the specified value.
+///
+/// Parameters:
+///  * `value` - the number to find the closest tick mark to.
+///
+/// Returns:
+///  * the number represented by the tick mark closest to the value provided to this method.
+///
+/// Notes:
+///  * Returns `value` if the slider has no tick marks
+///  * See also [hs._asm.guitk.element.slider:closestTickMark](#closestTickMark)
 static int slider_closestTickMarkValueToValue(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER, LS_TBREAK] ;
@@ -324,6 +499,54 @@ static int slider_closestTickMarkValueToValue(lua_State *L) {
     return 1 ;
 }
 
+/// hs._asm.guitk.element.slider:closestTickMark(value) -> integer
+/// Method
+/// Get the index of the tick mark closest to the specified value.
+///
+/// Parameters:
+///  * `value` - the number to find the closest tick mark to.
+///
+/// Returns:
+///  * the index of the the tick mark closest to the value provided to this method.
+///
+/// Notes:
+///  * Returns 0 if the slider has no tick marks
+///  * See also [hs._asm.guitk.element.slider:closestTickMarkValue](#closestTickMarkValue)
+static int slider_closestTickMarkToValue(lua_State *L) {
+    LuaSkin *skin = [LuaSkin shared] ;
+    [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER, LS_TBREAK] ;
+    HSASMGUITKElementSlider *slider = [skin toNSObjectAtIndex:1] ;
+    double value = [slider closestTickMarkValueToValue:lua_tonumber(L, 2)] ;
+
+    NSInteger matchIndex = 0 ;
+    NSInteger tickMarks = slider.numberOfTickMarks ;
+    if (tickMarks == 1) {
+        matchIndex = 1 ;
+    } else {
+        // c abhors checking doubles for equality, so check if difference is < 1/2 the difference
+        // between tick marks instead
+        double delta = (slider.maxValue - slider.minValue) / (2 * (tickMarks - 1)) ;
+
+        for (NSInteger i = 0 ; i < tickMarks ; i++) {
+            if (fabs(value - [slider tickMarkValueAtIndex:i]) < delta) {
+                matchIndex = i + 1 ;
+                break ;
+            }
+        }
+    }
+    lua_pushinteger(L, matchIndex) ;
+    return 1 ;
+}
+
+/// hs._asm.guitk.element.slider:rectOfTickMark(index) -> table
+/// Method
+/// Get the frame table of the tick mark at the specified index
+///
+/// Parameters:
+///  * `index` - an integer specifying the index of the tick mark to get the frame of
+///
+/// Returns:
+///  * a frame table specifying the tick mark's location within the element's frame. The frame coordinates will be relative to the top left corner of the slider's frame in it's parent.
 static int slider_rectOfTickMarkAtIndex(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TNUMBER | LS_TINTEGER, LS_TBREAK] ;
@@ -342,7 +565,39 @@ static int slider_rectOfTickMarkAtIndex(lua_State *L) {
     return 1 ;
 }
 
-// not really much use unless we add mouse tracking to elements and not just the manager
+/// hs._asm.guitk.element.slider:indexOfTickMarkAt(point) -> integer | nil
+/// Method
+/// Get the index of the tick mark closest to the specified point
+///
+/// Parameters:
+///  * `point` - a point table containing `x` and `y` coordinates of a point within the slider element's frame
+///
+/// Returns:
+///  * If the specified point is within the frame of a tick mark, returns the index of the matching tick mark; otherwise returns nil.
+///
+/// Notes:
+///  * It is currently not possible to invoke mouse tracking on just a single element; instead you must enable it for the manager the slider belongs to and calculate the point to compare by adjusting it to be relative to the slider elements top left point, e.g.
+/// ~~~lua
+///    g = require("hs._asm.guitk")
+///    w = g.new{ x = 100, y = 100, h = 100, w = 300 }:contentManager(g.manager.new()):show()
+///    m = w:contentManager():mouseCallback(function(mgr, message, point)
+///                              local geomPoint   = hs.geometry.new(point)
+///                              local slider      = mgr(1)
+///                              local sliderFrame = slider:frameDetails()._effective
+///                              if message == "move" and geomPoint:inside(sliderFrame) then
+///                                  local index = slider:indexOfTickMarkAt{
+///                                      x = point.x - sliderFrame.x,
+///                                      y = point.y - sliderFrame.y
+///                                  }
+///                                  if index then print("hovering over", index) end
+///                              end
+///                          end):trackMouseMove(true)
+///    m[1] = {
+///        _element = g.element.slider.new():tickMarks(10),
+///        frameDetails = { h = 100, w = 300 }
+///    }
+/// ~~~
+///  * A more efficient solution is being considered that would allow limiting tracking to only those elements one is interested in but there is no specific eta at this point.
 static int slider_indexOfTickMarkAtPoint(lua_State *L) {
     LuaSkin *skin = [LuaSkin shared] ;
     [skin checkArgs:LS_TUSERDATA, USERDATA_TAG, LS_TTABLE, LS_TBREAK] ;
@@ -440,7 +695,8 @@ static const luaL_Reg userdata_metaLib[] = {
     {"knobThickness",        slider_knobThickness},
     {"vertical",             slider_vertical},
     {"value",                slider_currentValue},
-    {"valueOfTickMark",      slider_tickMarkValueAtIndex},
+    {"tickMarkValue",        slider_tickMarkValueAtIndex},
+    {"closestTickMark",      slider_closestTickMarkToValue},
     {"closestTickMarkValue", slider_closestTickMarkValueToValue},
     {"rectOfTickMark",       slider_rectOfTickMarkAtIndex},
     {"indexOfTickMarkAt",    slider_indexOfTickMarkAtPoint},
@@ -485,10 +741,13 @@ int luaopen_hs__asm_guitk_element_slider(lua_State* L) {
         @"tickMarks",
         @"type",
         @"tickMarkLocation",
-        @"trackFillColor",
         @"vertical",
         @"value",
     ]] ;
+    if ([NSSlider instancesRespondToSelector:NSSelectorFromString(@"trackFillColor")]) {
+        lua_pushstring(L, "trackFillColor") ;
+        lua_rawseti(L, -2, luaL_len(L, -2) + 1) ;
+    }
     lua_setfield(L, -2, "_propertyList") ;
     lua_pushboolean(L, YES) ; lua_setfield(L, -2, "_inheritControl") ;
 //     lua_pushboolean(L, YES) ; lua_setfield(L, -2, "_inheritView") ;
