0024a2b763ae95339ecdcfa127754f91e5c3c31e
asmagill <asmagill@users.noreply.github.com>
2018-07-20 14:05:30 -0600

    more complete example with url paths supported in bonjour broadcast

--- bonjour/Examples/bonjourWebBrowser.lua
+++ bonjour/Examples/bonjourWebBrowser.lua
@@ -4,6 +4,11 @@ local module = {}
 
 local USERDATA_TAG = debug.getinfo(1).short_src:match("([^/]+)%.lua$")
 
+-- interesting, I didn't know this existed, but ultimately not of much use as it doesn't seem to have
+-- been utilized much... try it if you want
+local WIDE_AREA_DOMAIN = "dns-sd.org."
+local ENABLE_WIDE_AREA_DOMAIN = false
+
 local bonjour = require("hs._asm.bonjour")
 local chooser = require("hs.chooser")
 local host    = require("hs.host")
@@ -12,69 +17,110 @@ local hotkey  = require("hs.hotkey")
 
 -- make this whatever you want, or comment it out entirely
 hotkey.bind({"cmd", "alt", "ctrl"}, "=", function() module.toggle() end)
--- by wrapping module.toggle like this, it doesn't have to be defined yet as long as `module` has been
+
+-- ditto if you're using the wide area domain lookup as well
+if ENABLE_WIDE_AREA_DOMAIN then
+    hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "=", function() module.toggleWide() end)
+end
 
 local _log = require("hs.logger").new(USERDATA_TAG, "warning")
 
+local _chooserCommonCallback = function(item, serverList)
+    if item then
+        local link = item.subText
+        _log.f("open %s", link)
+        hs.execute("open " .. link)
+    end
+end
+
+local _chooserCommonChoices = function(serverList)
+    local options = {}
+    for k,v in fnutils.sortByKeys(serverList) do
+        local hostname, link = v:hostname(), "n/a"
+        if hostname then
+            local path = (v:txtRecord() or {}).path or "/"
+            link = string.format("http://%s:%d%s", hostname, v:port(), path)
+        end
+        table.insert(options, {
+            text    = v:name(),
+            subText = link,
+        })
+    end
+    return options
+end
+
 local _servers = {}
 
-local _chooser = chooser.new(function(item)
-        if item then
-            local site = _servers[item.text]
-            local link = "http://" .. site:hostname() .. ":" .. tostring(site:port())
-            _log.f("open %s", link)
-            hs.execute("open " .. link)
-        end
-    end):choices(function()
-        local options = {}
-        for k,v in fnutils.sortByKeys(_servers) do
-            table.insert(options, {
-                text    = v:name(),
-            })
-        end
-        return options
-    end)
+local _chooser = chooser.new(function(item) _chooserCommonCallback(item, _servers) end)
+                        :choices(function() return _chooserCommonChoices(_servers) end)
 
-local _browser = bonjour.browser.new():findServices("_http._tcp.", function(b, message, state, service, m)
-        if message == "service" then
-            local name = service:name()
-            _servers[name] = state and service or nil
-            _chooser:refreshChoicesCallback()
-            if state then
-                -- to get the port and hostname we have to resolve the address
-                _servers[name]:resolve(2, function(svc, msg, err)
-                    if msg == "stop" or msg == "resolved" then
-                        -- in case it disappeared on us, check that it's still around to stop
-                        if _servers[name] then _servers[name]:stop() end
-                    elseif msg == "error" then
-                        _log.ef("service resolve error for %s: %s", name, err)
-                    else
-                        _log.wf("unexpected resolve message for %s: %s", name, msg)
-                    end
-                end)
-            end
-        elseif message == "error" then
-            _log.ef("browser query error: %s", state)
+local _browserCommonCallback = function(serverList, ui, b, message, state, service, m)
+    if message == "service" then
+        local name = service:name()
+        serverList[name] = state and service or nil
+        if state then
+            -- to get the port and hostname we have to resolve the address
+            serverList[name]:resolve(10, function(svc, msg, err)
+                if msg == "stop" or msg == "resolved" then
+                    -- in case it disappeared on us, check that it's still around to stop
+                    if serverList[name] then serverList[name]:stop() end
+                elseif msg == "error" then
+                    _log.ef("service resolve error for %s: %s", name, err)
+                else
+                    _log.wf("unexpected resolve message for %s: %s", name, msg)
+                end
+                ui:refreshChoicesCallback()
+            end)
         else
-            _log.wf("unexpected browser query message: %s", message)
+            ui:refreshChoicesCallback() -- to clear removed entry
         end
-    end)
+    elseif message == "error" then
+        _log.ef("browser query error: %s", state)
+    else
+        _log.wf("unexpected browser query message: %s", message)
+    end
+end
+
+local _browser = bonjour.browser.new():findServices("_http._tcp.", function(...)
+    _browserCommonCallback(_servers, _chooser, ...)
+end)
 
 module.toggle = function()
     if _chooser:isVisible() then
         _chooser:hide()
     else
-        _chooser:bgDark(host.interfaceStyle() == "Dark")
-                :show()
+        _chooser:bgDark(host.interfaceStyle() == "Dark"):show()
+    end
+end
+
+if ENABLE_WIDE_AREA_DOMAIN then
+    local _serversWide = {}
+
+    local _chooserWide = chooser.new(function(item) _chooserCommonCallback(item, _serversWide) end)
+                            :choices(function() return _chooserCommonChoices(_serversWide) end)
+
+    local _browserWide = bonjour.browser.new():findServices("_http._tcp.", WIDE_AREA_DOMAIN, function(...)
+        _browserCommonCallback(_serversWide, _chooserWide, ...)
+    end)
+
+    module.toggleWide = function()
+        if _chooserWide:isVisible() then
+            _chooserWide:hide()
+        else
+            _chooserWide:bgDark(host.interfaceStyle() == "Dark"):show()
+        end
     end
 end
 
 -- keeps an inspect of the module clean, but this info is still available if I need it for debugging
 local _debug = {
     _log = _log,
-    _servers = _servers,
-    _browser = _browser,
-    _chooser = _chooser,
+    _servers     = _servers,
+    _serversWide = _serversWide,
+    _browser     = _browser,
+    _browserWide = _browserWide,
+    _chooser     = _chooser,
+    _chooserWide = _chooserWide,
 }
 
 return setmetatable(module, {
