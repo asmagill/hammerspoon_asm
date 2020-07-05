db60179fe273e1051e1bb6cf9a450a1e5f25ae20
asmagill <asmagill@users.noreply.github.com>
2020-07-05 00:05:13 -0500

    remove dead code

--- extras/init.lua
+++ extras/init.lua
@@ -9,9 +9,6 @@
 --     if not hs.window   then hs.window   = require("hs.window")   end
 
 -- maybe save some pain, if the shim is installed; otherwise, expect an objc dump to console when this loads on stock Hammerspoon without pull #2308 applied
-if package.searchpath("hs._asm.coroutineshim", package.path) then
-    require"hs._asm.coroutineshim"
-end
 
 -- package.loadlib("/System/Library/Frameworks/AddressBook.framework/Versions/A/AddressBook","*")
 local module = require("hs._asm.extras.internal")
